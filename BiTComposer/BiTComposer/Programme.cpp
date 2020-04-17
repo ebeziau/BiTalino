5c65809ffce0dbd66e62067839122ed82cf2024d
Eva <eva.beziau.1@ulaval.ce>
2020-04-17 19:34:06 +0200

    Analyse mouvements

--- BiTComposer/BiTComposer/Programme.cpp
+++ BiTComposer/BiTComposer/Programme.cpp
@@ -10,140 +10,134 @@
 using namespace std;
 #include <conio.h>
 
-#include "fmod_studio.hpp"
-#include "fmod_studio_common.h"
-#include "fmod_errors.h"
-#include "common.h"
+//#include "fmod_studio.hpp"
+//#include "fmod_studio_common.h"
+//#include "fmod_errors.h"
+//#include "common.h"
 
-
-
-int main()
+void fi()
 {
     // ------------------------------ - Jouer un son----------------------------
 
-    puts("Appuie sur Entrees quand tu es pret\n");
-    cin.ignore(300, '\n');
+    //puts("Appuie sur Entrees quand tu es pret\n");
+    //cin.ignore(300, '\n');
 
-    FMOD::System* system;
-    FMOD::Sound* sound1;
-    FMOD::Channel* channel = 0;
-    FMOD_RESULT       result;
-    unsigned int      version;
-    void* extradriverdata = 0;
+    //FMOD::System* system;
+    //FMOD::Sound* sound1;
+    //FMOD::Channel* channel = 0;
+    //FMOD_RESULT       result;
+    //unsigned int      version;
+    //void* extradriverdata = 0;
 
-    Common_Init(&extradriverdata);
+    //Common_Init(&extradriverdata);
 
-    /*
-        Create a System object and initialize
-    */
-    result = FMOD::System_Create(&system);
-    ERRCHECK(result);
+    ///*
+    //    Create a System object and initialize
+    //*/
+    //result = FMOD::System_Create(&system);
+    //ERRCHECK(result);
 
-    result = system->getVersion(&version);
-    ERRCHECK(result);
+    //result = system->getVersion(&version);
+    //ERRCHECK(result);
 
-    if (version < FMOD_VERSION)
-    {
-        Common_Fatal("FMOD lib version %08x doesn't match header version %08x", version, FMOD_VERSION);
-    }
+    //if (version < FMOD_VERSION)
+    //{
+    //    Common_Fatal("FMOD lib version %08x doesn't match header version %08x", version, FMOD_VERSION);
+    //}
 
-    result = system->init(32, FMOD_INIT_NORMAL, extradriverdata);
-    ERRCHECK(result);
+    //result = system->init(32, FMOD_INIT_NORMAL, extradriverdata);
+    //ERRCHECK(result);
 
-    result = system->createSound(Common_MediaPath("grease.wav"), FMOD_DEFAULT, 0, &sound1);
-    ERRCHECK(result);
+    //result = system->createSound(Common_MediaPath("grease.wav"), FMOD_DEFAULT, 0, &sound1);
+    //ERRCHECK(result);
 
-    result = sound1->setMode(FMOD_LOOP_OFF);    /* drumloop.wav has embedded loop points which automatically makes looping turn on, */
-    ERRCHECK(result);                           /* so turn it off here.  We could have also just put FMOD_LOOP_OFF in the above CreateSound call. */
-        
-    /*
-        Main loop
-    */
-    do
-    {
-        Common_Update();
+    //result = sound1->setMode(FMOD_LOOP_OFF);    /* drumloop.wav has embedded loop points which automatically makes looping turn on, */
+    //ERRCHECK(result);                           /* so turn it off here.  We could have also just put FMOD_LOOP_OFF in the above CreateSound call. */
+    //    
+    ///*
+    //    Main loop
+    //*/
+    //do
+    //{
+    //    Common_Update();
 
-        if (Common_BtnPress(BTN_ACTION1))
-        {
-            result = system->playSound(sound1, 0, false, &channel);
-            ERRCHECK(result);
-        }
+    //    if (Common_BtnPress(BTN_ACTION1))
+    //    {
+    //        result = system->playSound(sound1, 0, false, &channel);
+    //        ERRCHECK(result);
+    //    }
 
-        result = system->update();
-        ERRCHECK(result);
+    //    result = system->update();
+    //    ERRCHECK(result);
 
-        {
-            unsigned int ms = 0;
-            unsigned int lenms = 0;
-            bool         playing = 0;
-            bool         paused = 0;
-            int          channelsplaying = 0;
+    //    {
+    //        unsigned int ms = 0;
+    //        unsigned int lenms = 0;
+    //        bool         playing = 0;
+    //        bool         paused = 0;
+    //        int          channelsplaying = 0;
 
-            if (channel)
-            {
-                FMOD::Sound* currentsound = 0;
+    //        if (channel)
+    //        {
+    //            FMOD::Sound* currentsound = 0;
 
-                result = channel->isPlaying(&playing);
-                if ((result != FMOD_OK) && (result != FMOD_ERR_INVALID_HANDLE) && (result != FMOD_ERR_CHANNEL_STOLEN))
-                {
-                    ERRCHECK(result);
-                }
+    //            result = channel->isPlaying(&playing);
+    //            if ((result != FMOD_OK) && (result != FMOD_ERR_INVALID_HANDLE) && (result != FMOD_ERR_CHANNEL_STOLEN))
+    //            {
+    //                ERRCHECK(result);
+    //            }
 
-                result = channel->getPaused(&paused);
-                if ((result != FMOD_OK) && (result != FMOD_ERR_INVALID_HANDLE) && (result != FMOD_ERR_CHANNEL_STOLEN))
-                {
-                    ERRCHECK(result);
-                }
+    //            result = channel->getPaused(&paused);
+    //            if ((result != FMOD_OK) && (result != FMOD_ERR_INVALID_HANDLE) && (result != FMOD_ERR_CHANNEL_STOLEN))
+    //            {
+    //                ERRCHECK(result);
+    //            }
 
-                result = channel->getPosition(&ms, FMOD_TIMEUNIT_MS);
-                if ((result != FMOD_OK) && (result != FMOD_ERR_INVALID_HANDLE) && (result != FMOD_ERR_CHANNEL_STOLEN))
-                {
-                    ERRCHECK(result);
-                }
+    //            result = channel->getPosition(&ms, FMOD_TIMEUNIT_MS);
+    //            if ((result != FMOD_OK) && (result != FMOD_ERR_INVALID_HANDLE) && (result != FMOD_ERR_CHANNEL_STOLEN))
+    //            {
+    //                ERRCHECK(result);
+    //            }
 
-                channel->getCurrentSound(&currentsound);
-                if (currentsound)
-                {
-                    result = currentsound->getLength(&lenms, FMOD_TIMEUNIT_MS);
-                    if ((result != FMOD_OK) && (result != FMOD_ERR_INVALID_HANDLE) && (result != FMOD_ERR_CHANNEL_STOLEN))
-                    {
-                        ERRCHECK(result);
-                    }
-                }
-            }
+    //            channel->getCurrentSound(&currentsound);
+    //            if (currentsound)
+    //            {
+    //                result = currentsound->getLength(&lenms, FMOD_TIMEUNIT_MS);
+    //                if ((result != FMOD_OK) && (result != FMOD_ERR_INVALID_HANDLE) && (result != FMOD_ERR_CHANNEL_STOLEN))
+    //                {
+    //                    ERRCHECK(result);
+    //                }
+    //            }
+    //        }
 
-            system->getChannelsPlaying(&channelsplaying, NULL);
+    //        system->getChannelsPlaying(&channelsplaying, NULL);
 
-            Common_Draw("==================================================");
-            Common_Draw("Play Sound Example.");
-            Common_Draw("Copyright (c) Firelight Technologies 2004-2020.");
-            Common_Draw("==================================================");
-            Common_Draw("");
-            Common_Draw("Press %s to play a mono sound (drumloop)", Common_BtnStr(BTN_ACTION1));
-            Common_Draw("Press %s to quit", Common_BtnStr(BTN_QUIT));
-            Common_Draw("");
-            Common_Draw("Time %02d:%02d:%02d/%02d:%02d:%02d : %s", ms / 1000 / 60, ms / 1000 % 60, ms / 10 % 100, lenms / 1000 / 60, lenms / 1000 % 60, lenms / 10 % 100, paused ? "Paused " : playing ? "Playing" : "Stopped");
-            Common_Draw("Channels Playing %d", channelsplaying);
-        }
+    //        Common_Draw("==================================================");
+    //        Common_Draw("Play Sound Example.");
+    //        Common_Draw("Copyright (c) Firelight Technologies 2004-2020.");
+    //        Common_Draw("==================================================");
+    //        Common_Draw("");
+    //        Common_Draw("Press %s to play a mono sound (drumloop)", Common_BtnStr(BTN_ACTION1));
+    //        Common_Draw("Press %s to quit", Common_BtnStr(BTN_QUIT));
+    //        Common_Draw("");
+    //        Common_Draw("Time %02d:%02d:%02d/%02d:%02d:%02d : %s", ms / 1000 / 60, ms / 1000 % 60, ms / 10 % 100, lenms / 1000 / 60, lenms / 1000 % 60, lenms / 10 % 100, paused ? "Paused " : playing ? "Playing" : "Stopped");
+    //        Common_Draw("Channels Playing %d", channelsplaying);
+    //    }
 
-        Common_Sleep(50);
-    } while (!Common_BtnPress(BTN_QUIT));
+    //    Common_Sleep(50);
+    //} while (!Common_BtnPress(BTN_QUIT));
 
-    /*
-        Shut down
-    */
-    result = sound1->release();
-    ERRCHECK(result);
-    result = system->close();
-    ERRCHECK(result);
-    result = system->release();
-    ERRCHECK(result);
+    ///*
+    //    Shut down
+    //*/
+    //result = sound1->release();
+    //ERRCHECK(result);
+    //result = system->close();
+    //ERRCHECK(result);
+    //result = system->release();
+    //ERRCHECK(result);
 
-    Common_Close();
-
-
-
-
+    //Common_Close();
 
 
     /*FMOD_RESULT result;
@@ -174,9 +168,14 @@ int main()
     FMOD_System_Close(system);
     FMOD_System_Release(system);*/
 
-       //-------------------------------------------------------------------------
+    //-------------------------------------------------------------------------
 
 
+}
+
+int main()
+{
+    
     try
     {
         cout << "Salut Eva ! Je lance la recherche Bluetooth...\n";
@@ -202,10 +201,10 @@ int main()
         int deltaTemps = 0;
 
         //Acquisition acquisition;
-        int _maxCalibration(0);
-        int _minCalibration(500);
+        int _maxCalibration(340);
+        int _minCalibration(140);
 
-        puts("\n----------------------------CALIBRATION----------------------------------\n");
+        /*puts("\n----------------------------CALIBRATION----------------------------------\n");
         //acquisition.calibration(dev, frames);
 
         puts("2Nous allons proceder a la calibration. Tendez votre bras, paume vers l'exterieur, et ne bougez plus.");
@@ -242,7 +241,7 @@ int main()
 
         _getch();
 
-        puts("Calibration effectuee  !\n");
+        puts("Calibration effectuee  !\n");*/
         cout << "max : " << _maxCalibration << endl;
         cout << "min : " << _minCalibration << endl;
 
@@ -255,35 +254,48 @@ int main()
         puts("La musique va commencer ! Appuie sur Entree quand vous etes pret.\n");
         cin.ignore(300, '\n');
 
+        double valeurEnG;
+        int valeurAbs;
+        double MAX_GLOBAL = -5;
+        double MIN_GLOBAL = 5;
+
         int verif = 1;
         do {
             dev.read(frames);
             const BITalino::Frame f0 = frames[0];
             const BITalino::Frame f1 = frames[1];
 
-            //printf("f0.analog[0] : %d", f0.analog[0]);
-
-            double valeurEnG;
-            int valeurAbs;
+            //printf("f0.analog[0] : %d", f0.analog[0]);            
 
             //affichage en continu (changement d'affichage si changement dans la mesure (mouvement))
             int precedent = f0.analog[0];
             for (BITalino::Frame mesure : frames)
             {
                 valeurAbs = abs(mesure.analog[0] - precedent);
-                if (valeurAbs> 5)
+
+                printf("valeur absolue : %d \n", valeurAbs); //ecart entre 2 valeurs
+                valeurEnG = (2 * ((double)mesure.analog[0] - (double)_minCalibration) / ((double)_maxCalibration - (double)_minCalibration)) - 1;
+                printf("%d \n", mesure.analog[0]); //mesure analogique de l'accelerometre
+                printf("%lf \n\n", valeurEnG); //mesure en G.
+
+                if (valeurAbs > 10)
                 {
-                    printf("valeur absolue : %d \n", valeurAbs); //ecart entre 2 valeurs
-                    valeurEnG = (2 * (double)(mesure.analog[0] - _minCalibration) / (double)(_maxCalibration - _minCalibration)) - 1;
-                    printf("%d \n", mesure.analog[0]); //mesure analogique de l'accelerometre
-                    printf("%lf \n", valeurEnG); //mesure en G.
+
                     precedent = mesure.analog[0];
                 }
+
+                if (MAX_GLOBAL < valeurEnG) MAX_GLOBAL = valeurEnG;
+                if (MIN_GLOBAL > valeurEnG) MIN_GLOBAL = valeurEnG;
             }
+
+
             verif = verif + 1;
-        } while (!_kbhit());
+        }while (!_kbhit());
 
         //cout << verif << endl;
+
+        printf("MAX_GLOBAL : %lf \n", MAX_GLOBAL);
+        printf("MIN_GLOBAL : %lf \n", MIN_GLOBAL);
 
 
         dev.stop(); // stop acquisition
